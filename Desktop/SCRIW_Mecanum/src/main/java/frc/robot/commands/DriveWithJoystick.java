e82482e8d863e141b0a4e6b7485791a08a4dbb2a
Wren! <rehuggins14@gmail.com>
2024-09-12 17:14:24 -0400

    added code

--- Desktop/SCRIW_Mecanum/src/main/java/frc/robot/commands/DriveWithJoystick.java
+++ Desktop/SCRIW_Mecanum/src/main/java/frc/robot/commands/DriveWithJoystick.java
@@ -5,11 +5,24 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Joystick;
+import frc.robot.subsystems.DriveSystem.*;
+import frc.robot.subsystems.DriveSystem;
 
 public class DriveWithJoystick extends Command {
+
+  DriveSystem driveSystem;
+  Joystick joy;
+  
+  private double x;
+  private double y;
+  private double z;
   /** Creates a new DriveWithJoystick. */
-  public DriveWithJoystick() {
-    // Use addRequirements() here to declare subsystem dependencies.
+  public DriveWithJoystick(DriveSystem driveSystem, Joystick joy) {
+    addRequirements(driveSystem);
+    this.driveSystem = driveSystem;
+    this.joy = joy;
   }
 
   // Called when the command is initially scheduled.
@@ -18,15 +31,26 @@ public class DriveWithJoystick extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+
+    x = MathUtil.applyDeadband(joy.getX(), 0.15);
+    y = MathUtil.applyDeadband(joy.getY(), 0.15);
+    z = MathUtil.applyDeadband(joy.getZ(), 0.15);
+
+    driveSystem.drive(-x, y, -z / 2);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+
+    driveSystem.drive(0, 0, 0);
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    
     return false;
   }
 }
